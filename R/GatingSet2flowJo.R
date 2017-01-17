63645aff3c055b66519dc2102d9f62374087705c
Mike Jiang <wjiang2@fhcrc.org>
2017-01-17 20:34:42 +0000

    Revert "output logicle transformation as it is without converting it to biexp. #12"
    This reverts commit e0c74baca184ff4c8c9782c9b12f44c37aae0e8c.
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CytoML@125981 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GatingSet2flowJo.R
+++ R/GatingSet2flowJo.R
@@ -271,15 +271,14 @@ transformationNode <- function(gh, matInfo){
 
                 }else if(trans.type == "logicle"){
                   param <- as.list(environment(func))
-                  # withBasis <- - as.vector(param[["w"]]) * 4096
-                  withBasis <- as.vector(param[["w"]])
-                  transNode <- xmlNode("logicle"
+                  withBasis <- - as.vector(param[["w"]]) * 4096
+                  transNode <- xmlNode("biex"
                                        , namespace = "transforms"
                                        , attrs = c("transforms:length" = 4096
-                                                   , "transforms:T" = param[["t"]]
-                                                   , "transforms:A" = param[["a"]]
-                                                   , "transforms:W" = withBasis
-                                                   , "transforms:M" = param[["m"]]
+                                                   , "transforms:maxRange" = param[["t"]]
+                                                   , "transforms:neg" = param[["a"]]
+                                                   , "transforms:width" = withBasis
+                                                   , "transforms:pos" = param[["m"]]
                                        )
                   )
 

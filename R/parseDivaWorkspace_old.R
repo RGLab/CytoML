cb6f465eb352083f7af11d3a9c092de908e7aec7
mikejiang <wjiang2@fhcrc.org>
2018-06-25 11:54:49 -0700

    optimize parseDivaWorkspace

--- R/parseDivaWorkspace_old.R
+++ R/parseDivaWorkspace_old.R
@@ -1,207 +1,9 @@
-#' divaWorkspace class
-#' Inherited from \link{flowJoWorkspace-class}
-#' @importClassesFrom flowWorkspace flowJoWorkspace
-#' @exportClass divaWorkspace
-setClass("divaWorkspace", contains = "flowJoWorkspace")
-# copied from "openWorkspace" method (flowWorkspace/diva branch)
-#' open Diva xml workspace
-#'
-#' @param file xml file
-#' @param options argument passed to \link{xmlTreeParse}
-#' @param ... arguments passed to \link{xmlTreeParse}
-#' @return a \code{divaWorkspace} object
-#' @examples
-#' \dontrun{
-#' library(flowWorkspace)
-#' library(CytoML)
-#' ws <- openDiva(system.file('extdata/diva/PE_2.xml', package = "flowWorkspaceData"))
-#' ws
-#' getSampleGroups(ws)
-#' getSamples(ws)
-#' gs <- parseWorkspace(ws, name = 2, subset = 1)
-#' sampleNames(gs)
-#' getNodes(gs)
-#' plotGate(gs[[1]])
-#' }
-#' @export
-#' @importFrom XML xmlTreeParse xpathApply xmlGetAttr
-openDiva <- function(file,options = 0,...){
-  #message("We do not fully support all features found in a flowJo workspace, nor do we fully support all flowJo workspaces at this time.")
-  tmp<-tempfile(fileext=".xml")
-  if(!file.exists(file))
-    stop(file, " not found!")
-  if(!file.copy(file,tmp))
-    stop("Can't copy ", file, " to ", tmp)
-
-  if(inherits(file,"character")){
-    x<-xmlTreeParse(tmp,useInternalNodes=TRUE,options = options, ...);
-  }else{
-    stop("Require a filename of a workspace, but received ",class(x)[1]);
-  }
-  #    browser()
-  rootNode <- names(xmlChildren(x))
-
-  ver <- xpathApply(x, paste0("/", rootNode),function(x)xmlGetAttr(x,"version"))[[1]]
-  if(rootNode == "Workspace"){
-    x<-new("flowJoWorkspace",version=ver,.cache=new.env(parent=emptyenv()),file=basename(file),path=dirname(file),doc=x, options = as.integer(options))
-    x@.cache$flag <- TRUE
-  }else if(rootNode == "bdfacs"){
-    x <- new("divaWorkspace",version=ver,.cache=new.env(parent=emptyenv()),file=basename(file),path=dirname(file),doc=x, options = as.integer(options))
-    x@.cache$flag <- TRUE
-  }else
-    stop("Unrecognized xml root node: ", rootNode)
-
-  return(x);
-}
-
-#' @rdname divaWorkspace-class
-#' @param x divaWorkspace
-#' @importFrom flowWorkspace getSamples
-#' @export
-setMethod("getSamples","divaWorkspace",function(x){
-      selectMethod("getSampleGroups","divaWorkspace")(x)
-    })
-
-#' @rdname divaWorkspace-class
-#' @importFrom flowWorkspace getSampleGroups
-#' @export
-setMethod("getSampleGroups","divaWorkspace",function(x){
-      .getSampleGroupsDiva(x)
-    })
-
-#' @importFrom plyr ldply
-.getSampleGroupsDiva<-function(x){
-    ldply(
-        xpathApply(x@doc, "/bdfacs/experiment/specimen",function(specimen){
-              samples <- xpathApply(specimen, "tube",function(tube){
-                                            c(tube = xmlGetAttr(tube,"name")
-                                                , name = xmlValue(xmlElementsByTagName(tube,"data_filename")[[1]])
-                                            )
-                                  })
-
-              samples <- ldply(samples)
-              samples[["specimen"]] <- xmlGetAttr(specimen, "name")
-              samples
-            })
-      )
-
-}
-
-#' @rdname divaWorkspace-class
-#' @param object divaWorkspace
-#' @importFrom flowWorkspace show
-#' @export
-setMethod("show",c("divaWorkspace"),function(object){
-      cat("Diva Workspace Version ",object@version,"\n");
-      cat("File location: ",object@path,"\n");
-      cat("File name: ",object@file,"\n");
-      if(object@.cache$flag){
-        cat("Workspace is open.","\n");
-        cat("\nGroups in Workspace\n");
-
-        sg <- getSampleGroups(object)
-
-        tbl <- data.frame(table(sg$specimen))
-        colnames(tbl) <- c("specimen", "samples")
-        print(tbl)
-      }else{
-        cat("Workspace is closed.","\n")
-      }
-    })
-
-#' @rdname divaWorkspace-class
-#' @param obj divaWorkspace
-#' @param ... other arguments
-#' @importFrom flowWorkspace parseWorkspace
-#' @export
-setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
-      .preprocessorDiva(obj, ...)
-    })
-
-.preprocessorDiva<- function(obj, name = NULL
-                                    , subset = NULL
-                                    , path = obj@path
-                                    , fast = TRUE
-                                    , ...)
-{
-
-  #sample info
-  sg <- getSamples(obj)
-
-  # filter by group name
-  sg[["specimen"]] <- factor(sg[["specimen"]])
-  groups <- levels(sg[["specimen"]])
-
-  if(is.null(name)){
-    message("Choose which group of samples to import:\n");
-    groupInd <- menu(groups,graphics=FALSE);
-  }else if(is.numeric(name)){
-    if(length(groups)<name)
-      stop("Invalid sample group index.")
-    groupInd <- name
-  }else if(is.character(name)){
-    if(is.na(match(name,groups)))
-      stop("Invalid sample group name.")
-    groupInd <- match(name,groups)
-  }
-  group.name <- groups[groupInd]
-
-  sg <- subset(sg, specimen == group.name)
-#    browser()
-  #filter by subset (sample name or numeric index)
-  if(is.factor(subset)){
-    subset<-as.character(subset)
-  }
-  if(is.character(subset)){
-    sg <- subset(sg, name %in% subset)
-  }else if(is.numeric(subset))
-    sg <- sg[subset, ]
-
-  #check if there are samples to parse
-  sn <- sg[["name"]]
-  nSample <- length(sn)
-  if(nSample == 0)
-    stop("No samples in this workspace to parse!")
-
-
-  #check duplicated sample names
-
-  isDup <- duplicated(sn)
-  if(any(isDup))
-    stop("Duplicated sample names detected within group: ", paste(sampleSelected[isDup], collapse = " "), "\n Please check if the appropriate group is selected.")
-
-
-  message("Parsing ", nSample," samples");
-  if(fast)
-    .parseDivaWorkspace(xmlFileName=file.path(obj@path,obj@file)
-                      ,samples = sn
-                      , groupName = group.name
-                      ,path=path
-                      ,xmlParserOption = obj@options
-                      ,ws = obj
-                      ,...)
-  else
-    .parseDivaWorkspace_old(xmlFileName=file.path(obj@path,obj@file)
-                        ,samples = sn
-                        , groupName = group.name
-                        ,path=path
-                        ,xmlParserOption = obj@options
-                        ,ws = obj
-                        ,...)
-
-}
-
-#' @importFrom XML xpathSApply
-#' @importFrom flowCore read.FCS transformList spillover logicleTransform
-#' @importFrom flowWorkspace set.count.xml
-#' @importFrom ggcyto transform_gate
-#' @param scale_level indicates whether the gate is scaled by tube-level or gate-level biexp_scale_value (for debug purpose, May not be needed.)
-.parseDivaWorkspace <- function(xmlFileName,samples,path,xmlParserOption, ws, groupName, scale_level = "gate", verbose = FALSE, ...){
+.parseDivaWorkspace_old <- function(xmlFileName,samples,path,xmlParserOption, ws, groupName, scale_level = "gate", verbose = FALSE, ...){
 
   scale_level <- match.arg(scale_level, c("gate", "tube"))
   if(!file.exists(xmlFileName))
     stop(xmlFileName," not found!")
-#  gs <- new("GatingSet", guid = .uuid_gen(), flag = FALSE)
+  #  gs <- new("GatingSet", guid = .uuid_gen(), flag = FALSE)
 
 
   dataPaths <- vector("character")
@@ -240,10 +42,7 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
     stop("not sample to be added to GatingSet!")
 
   #load the raw data from FCS
-  fs <- read.ncdfFlowSet(files,isWriteSlice=FALSE,...)
-
-  gs <- GatingSet(fs)
-
+  fs <- read.ncdfFlowSet(files,isWriteSlice=TRUE,...)
   cnd <- colnames(fs)
 
   rootDoc <- ws@doc
@@ -257,8 +56,8 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
     min_val <- 2.6
   else
     stop("unsupported decade: ", log_decade)
-   # parse compp & trans
-  translist <- complist <- data.ranges <- list()
+  # parse compp & trans
+  translist <- complist <- list()
   for(file in files)
   {
 
@@ -284,9 +83,9 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
         #get biexp para
 
         biexp_para[[paramName]] <- c(min = as.numeric(xmlValue(xmlElementsByTagName(paramNode, "min")[[1]]))
-                                      , max = as.numeric(xmlValue(xmlElementsByTagName(paramNode, "max")[[1]]))
-                                      , biexp_scale = as.numeric(xmlValue(xmlElementsByTagName(paramNode, biexp_scale_node)[[1]]))
-                                      )
+                                     , max = as.numeric(xmlValue(xmlElementsByTagName(paramNode, "max")[[1]]))
+                                     , biexp_scale = as.numeric(xmlValue(xmlElementsByTagName(paramNode, biexp_scale_node)[[1]]))
+        )
         #get comp
         coef <- as.numeric(xpathSApply(paramNode, "compensation/compensation_coefficient", xmlValue))
         # browser()
@@ -295,7 +94,7 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
 
       }else
         res <- NULL
-        return(res)
+      return(res)
     }, biexp_para = biexp_para)
     #comp stored in xml seems to be incorrect
 
@@ -303,11 +102,10 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
     #parse comp
     ##################################
 
-
-    message("loading data: ",file);
-    data <- read.FCS(file)[, cnd]
-
-    message("Compensating")
+    data <- fs[[sampleName]]
+    # message("loading data: ",file);
+    # data <- read.FCS(file)[, cnd]
+    #
     #we use the spillover from FCS keyword
     comp <- spillover(data)
     comp <- compact(comp)
@@ -317,14 +115,6 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
       stop("No spillover found in FCS!")
     else
       complist[[sampleName]] <- comp[[1]]
-
-    data <- compensate(data, comp[[1]])
-
-    message("computing data range")
-    data.ranges[[sampleName]] <- range(data, "data")
-
-
-    message(paste("transforming ..."))
 
     #parse trans
     params <- names(biexp_para)
@@ -336,19 +126,29 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
       pos <- 4.5
       r <- abs(this_para[["biexp_scale"]])
       trans <- generate_trans(maxValue, pos, r)
-                }
-      , simplify = FALSE)
+    }
+    , simplify = FALSE)
+
     translist[[sampleName]] <- transformerList(params, trans)
 
-    trans.funs <- sapply(trans, function(trans.obj)trans.obj[["transform"]])
-    data <- transform(data, transformList(params, trans.funs))
+  }
 
 
-    message("parsing gates ...")
+  gs <- GatingSet(fs)
+  message("Compensating")
+  gs <- compensate(gs, complist)
 
-    gh <- gs[[sampleName]]
+  message("computing data range")
+  data.ranges <- sapply(sampleNames(gs), function(sn)range(getData(gs[[sn]]), "data"), simplify = FALSE)
 
-    this_biexp <- translist[[sampleName]]
+  message(paste("transforming ..."))
+  gs <- transform(gs, translist)
+
+  message("parsing gates ...")
+  for(sn in sampleNames(gs)){
+    gh <- gs[[sn]]
+
+    this_biexp <- translist[[sn]]
     xpathSample <- paste0(xpathGroup, "/tube[data_filename='", sampleName, "']")
     sampleNode <- xpathApply(rootDoc, xpathSample)[[1]]
     #assume the gates listed in xml follows the topological order
@@ -477,7 +277,7 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
             bound[xParam, ] <- c(min_val, 262143)
           if(y.extend)
             bound[yParam, ] <- c(min_val, 262143)
-          gate <- extend(gate, bound, t(data.ranges[[sampleName]]))
+          gate <- extend(gate, bound, t(data.ranges[[sn]]))
           #need transform since extention was performed on the raw-scale gate
           if(x.extend)
             gate <- transform_gate(gate, x_biexp@.Data, xParam)
@@ -492,11 +292,7 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
         if(parent == "root")
           parent <- ""
         unique.path <- file.path(parent, nodeName)
-        #Can't do the gating in
-        # ind <- getIndices(gh, parent)
-        # ind <- as.logic(Subset(data[ind, ], gate))
-        # updateIndices(gh, unique.path, ind)
-        # suppressMessages(recompute(gh, unique.path))
+        suppressMessages(recompute(gh, unique.path))
         #save the xml counts
         set.count.xml(gh, unique.path, count)
       }else{
@@ -506,14 +302,12 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
         set.count.xml(gh, "root", count)
         next
       }
-    }#end of gate adding
-    fs[[sampleName]] <- data
+
+
+    }
+
 
   }
-  gs@compensation <- complist
-
-  gs@transformation <- translist
-  suppressMessages(recompute(gs))
 
 
 
@@ -526,29 +320,17 @@ setMethod("parseWorkspace",signature("divaWorkspace"),function(obj, ...){
   #we don't want to return the splitted gs since they share the same cdf and externalptr
   #thus should be handled differently(more efficiently) from the regular gslist
 
-#    # try to post process the GatingSet to split the GatingSets(based on different the gating trees) if needed
+  #    # try to post process the GatingSet to split the GatingSets(based on different the gating trees) if needed
   gslist <- suppressMessages(flowWorkspace:::.groupByTree(gs))
   if(length(gslist) > 1)
     warning("GatingSet contains different gating tree structures and must be cleaned before using it! ")
-#    if(length(gslist) == 1){
-#      return (gslist[[1]])
-#    }else
+  #    if(length(gslist) == 1){
+  #      return (gslist[[1]])
+  #    }else
   {
-#      warning("Due to the different gating tree structures, a list of GatingSets is returned instead!")
-#      return (gslist)
-  }
+    #      warning("Due to the different gating tree structures, a list of GatingSets is returned instead!")
+    #      return (gslist)
+    }
   gs
 
-}
-
-#use the equation suggested by BD engineer last year
-#' @importFrom flowWorkspace logicle_trans
-generate_trans <- function(maxValue = 262144, pos = 4.5, r)
-{
-  if(r == 0)
-    r <- maxValue/10^pos
-  w <- (pos - log10(maxValue/r))/2
-  if(w < 0)
-    w <- 0
-  logicle_trans(w=w, t = maxValue, m = pos) #
 }

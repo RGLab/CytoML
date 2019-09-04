74fd91fcbbffe1f292b7e77f7a6cd9a11e14652a
mikejiang <wjiang2@fhcrc.org>
2019-09-04 13:35:41 -0700

    lower precision for format_float

--- R/GatingSet2flowJo.R
+++ R/GatingSet2flowJo.R
@@ -735,7 +735,7 @@ gateAttr <- function(eventsInside){
 #customize precision for double to match up to pugixml behavior
 format_float <- function(x){
   if(!is.null(x))
-    x <- format(x, digits = 17)
+    x <- format(x, digits = 10)
   x
 }
 #modified based on xmlDimensionNode

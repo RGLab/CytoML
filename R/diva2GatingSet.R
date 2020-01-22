ae3e7df47bace216fb9c0142364aa1c939198f80
Jacob Wagner <jpwagner@fredhutch.org>
2020-01-22 11:09:37 -0800

    Import flowWorkspace::merge_list_to_gs

--- R/diva2GatingSet.R
+++ R/diva2GatingSet.R
@@ -296,7 +296,7 @@ diva_to_gatingset<- function(obj, name = NULL
 }
 #' @importFrom XML xpathSApply
 #' @importFrom flowCore read.FCS transformList spillover logicleTransform
-#' @importFrom flowWorkspace gh_pop_set_xml_count save_gs load_gs gs_split_by_tree fix_channel_slash compute_timestep gh_pop_is_hidden gh_pop_is_negated swap_data_cols cs_swap_colnames get_cytoframe_from_cs load_cytoframe_from_h5 cf_write_h5
+#' @importFrom flowWorkspace gh_pop_set_xml_count save_gs load_gs gs_split_by_tree fix_channel_slash compute_timestep gh_pop_is_hidden gh_pop_is_negated swap_data_cols cs_swap_colnames get_cytoframe_from_cs load_cytoframe_from_h5 cf_write_h5 merge_list_to_gs
 #' @importFrom ggcyto rescale_gate
 #' @param scale_level indicates whether the gate is scaled by tube-level or gate-level biexp_scale_value (for debug purpose, May not be needed.)
 #' @noRd
